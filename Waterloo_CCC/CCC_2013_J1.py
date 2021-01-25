fecb7c7445f81eb2fe6d2acfb6247559a74608f1
Paul Miskew <pmiskew@C1MWP4S2J1WL.local>
2021-01-25 08:27:47 -0500

    update

---
+++ Waterloo_CCC/CCC_2013_J1.py
@@ -0,0 +1,16 @@
+#Step 1: Take inputs
+y = input()
+y = int(y)
+
+m = input()
+m = int(m)
+
+#Step 2: Find the difference between youngest and middle
+d = m - y # 15 - 12 = 3
+
+#Step 3: Find oldest by adding d to middle age
+o = m + d
+
+#Step 4: print out result
+print(o)
+
